15726e735c62caf2a7e5341095ca929a68521c22
Ruben Perez <ruben.perezjuarez@mail.mcgill.ca>
2023-05-13 16:19:52 -0400

    final touch

--- 01_instructions/04_displaying_api_response_UI.md
+++ 01_instructions/04_displaying_api_response_UI.md
@@ -10,6 +10,14 @@ It all works great
 
 ## Add the Search City input to render dynamically
 
-1. In `app.component.html` create another div to add the Search input.
+1. In `app.component.html` create another div to add the Search input. Syle this div.
 
-2. Syle this div.
+2. Now we need to import `FormsModule` in `app.module.ts`.
+
+3. Go to `app.component.html` and add a tag to the form `#ngForm` and add the onSubmit method `(submit)="onSubmit"`.
+
+4. Also add a name property to the `input`: ` name="city" [(ngModel)]="cityName"`. After adding this, i can now use this variable in my following function.
+
+5. in `app.components.ts` cut the content of the INIT function and create another function below called `private` and paste it there.
+
+6. Now let's create this component in `app.components.ts`. Below `ngOnInit()` write this method/function: `onSubmit(){}`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,33 @@ import { WeatherData } from './models/weather.model';
 export class AppComponent implements OnInit {
     constructor(private weatherService: WeatherService) {}
 
+    // this is the starting value of the variable, when the page loads 1st time
+    cityName: string = 'Montréal';
     weatherData?: WeatherData;
 
+    // function fires when website loads
     ngOnInit(): void {
-        this.weatherService.getWeatherData('London').subscribe({
+        // this calls the function passing Montreal, which is the default
+        this.getWeatherData(this.cityName);
+        // this cleans the name from the input /search bar
+        this.cityName = '';
+    }
+
+    // when hitting SUBMIT in the INPUT
+    onSubmit() {
+        // this calls the function passing the city in the search bar
+        this.getWeatherData(this.cityName);
+        // this cleans the name from the input /search bar
+
+        this.cityName = '';
+    }
+
+    // function that calls the API
+    private getWeatherData(cityName: string) {
+        this.weatherService.getWeatherData(cityName).subscribe({
             next: (response) => {
-                console.log('this is the response');
-                console.log(response);
+                // console.log('this is the response');
+                // console.log(response);
                 this.weatherData = response;
             },
         });
